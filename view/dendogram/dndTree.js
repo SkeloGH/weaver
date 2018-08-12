3abc2275621f289e74f7c26a3d7c0bc4a4b421bc
Aaron_GH <aaron@rigs.com.mx>
2018-08-11 21:32:41 -0500

    exclude node_modules, and fixed reference in dendograms script

--- view/dendogram/dndTree.js
+++ view/dendogram/dndTree.js
@@ -27,7 +27,7 @@ EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.*/
 
 
 // Get JSON data
-treeJSON = d3.json("/dendogram/data.out.json", function(error, treeData) {
+treeJSON = d3.json("./dendogram/data.out.json", function(error, treeData) {
 
     // Calculate total nodes, max label length
     var totalNodes = 0;
