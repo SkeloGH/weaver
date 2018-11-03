76bddd6c67a9c3458cf1c4a7b588947dab3843c7
Aaron_GH <aaron@rigs.com.mx>
2018-11-03 10:37:03 -0600

    merge

--- approach_004-schemaless/config/index.js
+++ approach_004-schemaless/config/index.js
@@ -7,7 +7,6 @@ const ObjectId = mongo.ObjectID;
 module.exports = {
   queries: [
     { _id: ObjectId("58dd856c1a70665027a41183") /* example id, don't panic */ },
-    { _id: ObjectId("5afc95fcb8e8dc047658ab49") /* example id, don't panic */ },
   ],
   dataClients: dataClients,
   jsonConfig: {
