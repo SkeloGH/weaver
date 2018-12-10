88576dc64f7bf0a9727f9ec29e12a243c0d97645
Aaron_GH <aaron@rigs.com.mx>
2018-12-10 00:29:32 -0600

    added, ignoreFields usage example

--- approach_004-schemaless/config/secret.example.js
+++ approach_004-schemaless/config/secret.example.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
 /**
+ * Preferably, add the .out extension to the ignoreFields file to avoid committing into source control.
+ * */
+const ignoreFields = require('./ignoreFields');
+/**
 Example secret config file
 */
 module.exports = {
@@ -11,28 +15,31 @@ module.exports = {
         { name: 'local-animals-db' },
         { name: 'local-shelters-db' }
       ],
-    }
-  },
-  ssh: {
-    port: 22,
-    private_key: fs.readFileSync('/Users/john_doe/.ssh/id_rsa'),
-    agent: process.env.SSH_AUTH_SOCK,
-    username: process.env.USER,
-  },
-  remote: {
-    db: {
-      url: 'mongodb://localhost:27017',
-      sources: [
-        { name: 'production-animals-db' },
-        { name: 'production-shelters-db' }
-      ],
     },
     client: {
-      ignoreFields: ['donations', 'passwords']
-    },
-    host: 'production.hostname.com',
-    dstPort: 27017,
-    localPort: 27017,
-    localhost: '127.0.0.1',
-  }
+      ignoreFields: ignoreFields
+    }
+  },
+  // ssh: {
+  //   port: 22,
+  //   private_key: fs.readFileSync('/Users/john_doe/.ssh/id_rsa'),
+  //   agent: process.env.SSH_AUTH_SOCK,
+  //   username: process.env.USER,
+  // },
+  // remote: {
+  //   db: {
+  //     url: 'mongodb://localhost:27017',
+  //     sources: [
+  //       { name: 'production-animals-db' },
+  //       { name: 'production-shelters-db' }
+  //     ],
+  //   },
+  //   client: {
+  //     ignoreFields: ['donations', 'passwords']
+  //   },
+  //   host: 'production.hostname.com',
+  //   dstPort: 27017,
+  //   localPort: 27017,
+  //   localhost: '127.0.0.1',
+  // }
 };
