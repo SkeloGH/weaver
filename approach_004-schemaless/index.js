d33336a45bc5038672f7af720c97f43f18d18a5a
Aaron_GH <aaron@rigs.com.mx>
2018-12-28 14:10:57 -0600

    added, comments on main app file

--- approach_004-schemaless/index.js
+++ approach_004-schemaless/index.js
@@ -1,15 +1,24 @@
-const fs       = require('fs');
-const async    = require('async');
 const logging  = require('debug');
-const ld       = {
-  array: require('lodash/array'),
-  object: require('lodash/object'),
-};
 
 const WeaverCollect = require('./collect');
 const WeaverDigest = require('./digest');
 
+/**
+  *
+  * The main API class
+  * @class Weaver
+*/
 class Weaver {
+/**
+  *
+  * Consumes the given configuration object and initializes dependencies.
+  * @constructor
+  * @param {Object} config
+  * @param {Array.<Object>} config.queries
+  * @param {Array.<WeaverMongoClient>} config.dataClients
+  * @param {Object} config.jsonConfig
+  * @param {string} config.jsonConfig.filePath
+*/
   constructor(config) {
     this.__cache = {};
     this.collect = new WeaverCollect(config);
@@ -17,6 +26,16 @@ class Weaver {
     return this._configure(config);
   }
 
+/**
+  *
+  * Assigns the configuration object values to class properties.
+  * @param {Object} config
+  * @param {Array.<Object>} config.queries
+  * @param {Array.<WeaverMongoClient>} config.dataClients
+  * @param {Object} config.jsonConfig
+  * @param {string} config.jsonConfig.filePath
+  * @returns {this} this
+*/
   _configure(config) {
     this.logging     = logging(`Weaver`);
     this.queries     = config.queries;
@@ -27,6 +46,12 @@ class Weaver {
     return this;
   }
 
+/**
+  *
+  * Given a results object, uses the standad output to print the results.
+  * @param {Object} results
+  * @returns {Promise.<Object>}
+*/
   showResults = (results) => {
     const dataEntries = Object.keys(results);
     this.logging(`Found interlaced dataEntries: ${dataEntries.join(', ')}`);
@@ -34,12 +59,24 @@ class Weaver {
     return Promise.resolve(results);
   }
 
+/**
+  *
+  * Given a list of clients, initialize their connections by calling their own connect method.
+  * @param {Array.<WeaverMongoClient>} clients
+  * @returns {Promise.<Object>}
+*/
   connectClients = (clients) => {
     return Promise.all(
       clients.map(client => client.connect())
     ).catch(this.logging);
   }
 
+/**
+  *
+  * Given a list of clients, initialize their connections by calling their own connect method.
+  * @param {CallableFunction} cb
+  * @returns {undefined}
+*/
   run = (cb) => {
     this.connectClients(this.dataTargets)
       .then(() => this.connectClients(this.dataSources))
@@ -53,7 +90,10 @@ class Weaver {
   }
 }
 
-/** @TODO detect if running as module or expect query in script arguments */
+/**
+  * Detects if being called as module, otherwise initializes the app.
+  * @TODO delegate initialization to external module consumer
+*/
 if (require.main === module) {
   new Weaver(require('./config')).run((err) => {
     logging('Done');
