e3ac704cb74e4220bf3f9014bfaa6e6f2a513be4
Aaron_GH <aaron@rigs.com.mx>
2019-04-29 22:57:00 -0500

    added, basic Weaver methods coverage, except run

--- approach_004-schemaless/__tests__/index.test.js
+++ approach_004-schemaless/__tests__/index.test.js
@@ -1,10 +1,52 @@
-import Weaver from '../'
-import * as CONFIG from '../config'
+import Weaver from '../';
+require("@babel/polyfill");
+
+const mockClient = (type) => {
+  return { config: { type }, connect() { return Promise.resolve('works') } }
+};
+const mockClients = [ mockClient('source'), mockClient('target') ];
+const CONFIG = {
+  queries: [],
+  dataClients: [...mockClients],
+  jsonConfig: {
+    filePath: `${process.env.PWD}/results/weaver.test.json`
+  }
+};
 
 test('Public methods are defined', () => {
-  const instance = new Weaver(CONFIG);
-  expect(instance._configure).not.toBeUndefined();
-  expect(instance.showResults).not.toBeUndefined();
-  expect(instance.connectClients).not.toBeUndefined();
-  expect(instance.run).not.toBeUndefined();
+  const weaver = new Weaver(CONFIG);
+  expect(weaver.collect).not.toBeUndefined();
+  expect(weaver.digest).not.toBeUndefined();
+  expect(weaver._configure).not.toBeUndefined();
+  expect(weaver.showResults).not.toBeUndefined();
+  expect(weaver.connectClients).not.toBeUndefined();
+  expect(weaver.run).not.toBeUndefined();
+});
+
+test('Self class configuration', () => {
+  const weaver = new Weaver(CONFIG);
+  expect(weaver.queries).not.toBeUndefined();
+  expect(weaver.dataClients).not.toBeUndefined();
+  expect(weaver.jsonConfig).not.toBeUndefined();
+  expect(weaver.dataSources).not.toBeUndefined();
+  expect(weaver.dataTargets).not.toBeUndefined();
+});
+
+test('"showResults" returns the same input', async () => {
+  const weaver = new Weaver(CONFIG);
+  const mockObj = { a: 'b', "c": "d" };
+  const results = await weaver.showResults(mockObj);
+  expect(results).toBe(mockObj)
+});
+
+test('"connectClients" calls the connect methods', async () => {
+  const weaver = new Weaver(CONFIG);
+  const client = { connect() { return Promise.resolve('works') } };
+  const mockClients = [client];
+  const results = await weaver.connectClients(mockClients);
+  expect(results).toEqual(['works'])
+});
+
+test('"run" method proper execution', () => {
+  // TODO: refactor method for testability
 });
