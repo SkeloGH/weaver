5ddf978d14ff56f2350fe17c6cdf1ff187759e71
Aaron_GH <aaron@rigs.com.mx>
2019-12-08 00:32:40 -0600

    use .eslintignore to ignore dist directory from liniting

--- jest.config.js
+++ jest.config.js
@@ -28,9 +28,9 @@ module.exports = {
   coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  coveragePathIgnorePatterns: [
+    '/dist/',
+  ],
 
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
