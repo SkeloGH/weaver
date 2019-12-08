45e5c7773ff34b76b98bc12f07c8098efbf04b28
Aaron_GH <dev.aaron.gh@gmail.com>
2019-12-08 14:58:24 -0600

    validate config is correct before running

--- dist/bin/commands/run.js
+++ dist/bin/commands/run.js
@@ -20,16 +20,19 @@ module.exports = {
     const config = getConfig(_argv);
     const configStr = JSON.stringify(config, null, 2);
     let message = `Running with the current configuration"\n ${configStr}`;
+    const hasQueries = config.queries && config.queries.length > 0;
+    const hasDataClients = config.dataClients && config.dataClients.length > 0;
+    const validConfig = hasDataClients && hasQueries;
 
-    if (!config.dataClients || !config.dataClients.length) {
+    if (!hasDataClients) {
       message = `Error: dataClients not set, try adding them:
       - To the configuration file, or
       - Using the configuration wizard
       `;
-      return shell.echo(message);
     }
 
     shell.echo(message);
+    if (!validConfig) return _argv;
     const weaver = new Weaver(config);
     weaver.run(result => {
       logging('Result', result);
