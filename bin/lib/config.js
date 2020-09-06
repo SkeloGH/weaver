67eefbd54483dc871456e4bc04b320ccbc3fa774
Aaron Gonzalez <SkeloGH@users.noreply.github.com>
2020-09-06 15:06:55 -0500

    audit fix and getCLIJSONContent  returns empty object if no config is found (#89)

--- bin/lib/config.js
+++ bin/lib/config.js
@@ -12,7 +12,7 @@ let CLI_ARGS = {};
 
 const readConfigFile = () => {
   let config = {};
-  const CLI_CONFIG = getCLIJSONContent() || {};
+  const CLI_CONFIG = getCLIJSONContent();
   const path = CLI_CONFIG.filePath;
   const isValid = validateConfig(path).valid;
   if (path && isValid) {
