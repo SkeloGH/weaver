2cfe0302fa024067de0884e6dbc9beee58d750a3
SkeloGH <dev.aaron.gh@gmail.com>
2022-10-28 01:50:09 -0500

    turned off autostart, changed debug to strings

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -8,7 +8,7 @@ module.exports = {
       dbName: 'weaver--jest',
       // dbPath?: string, // by default create in temp directory
       // storageEngine?: string, // by default `ephemeralForTest`, available engines: [ 'devnull', 'ephemeralForTest', 'mmapv1', 'wiredTiger' ]
-      debug: true, // by default false
+      debug: 'true', // by default false
       // replSet?: string, // by default no replica set, replica set name
       // auth?: boolean, // by default `mongod` is started with '--noauth', start `mongod` with '--auth'
       // args?: string[], // by default no additional arguments, any additional command line arguments for `mongod` `mongod` (ex. ['--notablescan'])
@@ -19,14 +19,14 @@ module.exports = {
       // downloadDir?: string, // by default node_modules/.cache/mongodb-memory-server/mongodb-binaries
       // platform?: string, // by default os.platform()
       // arch?: string, // by default os.arch()
-      debug: true, // by default false
+      debug: 'true', // by default false
       // checkMD5?: boolean, // by default false OR process.env.MONGOMS_MD5_CHECK
       skipMD5: true,
       // systemBinary?: string, // by default undefined or process.env.MONGOMS_SYSTEM_BINARY
     },
     // autoStart?: boolean, // by default true
-    autoStart: true,
-    debug: true, // by default false
+    autoStart: false,
+    debug: 'true', // by default false
     debug: process.env.DEBUG === '*',
   },
 };
