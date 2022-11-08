24d0dffb617ba7e46fe38e3e05cbe1459f3a6f26
SkeloGH <dev.aaron.gh@gmail.com>
2022-11-07 23:06:37 -0600

    decoplued argument parsing from run command, and added base test

--- bin/__tests__/commands/run.test.js
+++ bin/__tests__/commands/run.test.js
@@ -1,7 +1,15 @@
 const run = require('../../commands/run');
 
 describe('weaver run command tests', () => {
-  test('Base behavior', () => {
+  test('Command has the expected properties', () => {
     expect(run).not.toBe(undefined);
+    expect(run.name).not.toBe(undefined);
+    expect(run.description).not.toBe(undefined);
+    expect(run.setup).toBeInstanceOf(Function);
+    expect(run.parse).toBeInstanceOf(Function);
+  });
+
+  test('Command parsing', () => {
+    expect(run.parse({})).toMatchObject({});
   });
 });
