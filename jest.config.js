e1e68c39fe5245693e6dfb2bd092ccecab39a22e
SkeloGH <dev.aaron.gh@gmail.com>
2022-11-13 13:46:21 -0600

    test jest config

--- jest.config.js
+++ jest.config.js
@@ -71,14 +71,14 @@ module.exports = {
   // ],
 
   // An array of file extensions your modules use
-  // moduleFileExtensions: [
-  //   "js",
+  moduleFileExtensions: [
+    "js",
   //   "json",
   //   "jsx",
   //   "ts",
   //   "tsx",
   //   "node"
-  // ],
+  ],
 
   // A map from regular expressions to module names that allow to stub out resources with a single module
   // moduleNameMapper: {},
