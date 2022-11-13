7cfdb08b3f6e3745ccfef7272b0947bddf44dac2
SkeloGH <dev.aaron.gh@gmail.com>
2022-11-13 16:27:30 -0600

    decoupled and enhanced config validation

--- bin/__tests__/commands/run.test.js
+++ bin/__tests__/commands/run.test.js
@@ -1,4 +1,8 @@
 const run = require('../../commands/run');
+const {
+  _isValidConfig,
+  parse,
+} = require('../../commands/run/index');
 
 describe('weaver run command tests', () => {
   test('Command has the expected properties', () => {
@@ -10,6 +14,32 @@ describe('weaver run command tests', () => {
   });
 
   test('Command parsing', () => {
-    expect(run.parse({})).toMatchObject({});
+    expect(parse).toBeInstanceOf(Function);
+    expect(parse({})).toMatchObject({});
+  });
+  test('Command options validation', () => {
+    const valid = {
+      queries: ['507f1f77bcf86cd799439011'],
+      dataClients: [{
+        type: 'source',
+        db: {
+          url: 'test://',
+          name: 'test',
+        },
+      }],
+    };
+    const invalid1 = { queries: [], dataClients: [] };
+    const invalid2 = { queries: {}, dataClients: {} };
+    const invalid3 = { queries: 'any', dataClients: 'any' };
+    const invalid4 = { other: 'value' };
+    const expectedWhenValid = { validConfig: true, hasQueries: true, hasDataClients: true };
+    const expectedWhenInvalid = { validConfig: false, hasQueries: false, hasDataClients: false };
+
+    expect(_isValidConfig).toBeInstanceOf(Function);
+    expect(_isValidConfig(invalid1)).toMatchObject(expectedWhenInvalid);
+    expect(_isValidConfig(invalid2)).toMatchObject(expectedWhenInvalid);
+    expect(_isValidConfig(invalid3)).toMatchObject(expectedWhenInvalid);
+    expect(_isValidConfig(invalid4)).toMatchObject(expectedWhenInvalid);
+    expect(_isValidConfig(valid)).toMatchObject(expectedWhenValid);
   });
 });
--- bin/commands/run/index.js
+++ bin/commands/run/index.js
@@ -1,7 +1,9 @@
 const Debug = require('debug');
 const shell = require('shelljs');
 const ObjectId = require('bson-objectid');
-const ldLang = require('lodash');
+const ldLang = require('lodash/lang');
+const ldObject = require('lodash/object');
+const ldColl = require('lodash/collection');
 
 const {
   getConfig,
@@ -14,18 +16,48 @@ const clientsByFamily = {
   mongodb: WeaverMongoClient,
 };
 
+const _hasValidQueries = (queries) => {
+  let validQueries = ldLang.isArray(queries);
+  validQueries = validQueries && !ldLang.isEmpty(queries);
+  validQueries = validQueries && !ldLang.isEmpty(queries[0]);
+  validQueries = validQueries && ldColl.every(queries, ObjectId.isValid);
+  return validQueries;
+};
+
+const _hasValidDataClient = (dataClient) => {
+  let validDataClient = ldLang.isObject(dataClient);
+  validDataClient = validDataClient && ldObject.hasIn(dataClient, 'type');
+  validDataClient = validDataClient && (dataClient.type === 'target' || dataClient.type === 'source');
+  validDataClient = validDataClient && ldObject.hasIn(dataClient, 'db');
+  validDataClient = validDataClient && ldObject.hasIn(dataClient, 'db.url');
+  validDataClient = validDataClient && ldObject.hasIn(dataClient, 'db.name');
+  return validDataClient;
+};
+
+const _hasValidDataClientss = (dataClients) => {
+  let validDataClients = ldLang.isArray(dataClients);
+  validDataClients = validDataClients && !ldLang.isEmpty(dataClients);
+  validDataClients = validDataClients && ldColl.every(dataClients, _hasValidDataClient);
+  return validDataClients;
+};
+
+const _isValidConfig = (config) => {
+  const hasQueries = _hasValidQueries(config.queries);
+  const hasDataClients = _hasValidDataClientss(config.dataClients);
+  const validConfig = hasDataClients && hasQueries;
+  return { hasQueries, hasDataClients, validConfig };
+};
+
 const parse = (_argv) => {
   logging(`getting config from argv ${_argv}`);
   const config = getConfig(_argv);
   const configStr = JSON.stringify(config, null, 2);
   let message = `Running with the current configuration"\n ${configStr}`;
-  const hasQueries = !ldLang.isEmpty(config.queries);
-  const hasDataClients = !ldLang.isEmpty(config.dataClients);
-  const validConfig = hasDataClients && hasQueries;
+  const { validConfig, hasQueries, hasDataClients } = _isValidConfig(config);
 
   if (!hasDataClients) {
     message = `Error: dataClients not set, try:
-    weaver add [client|query|ignore]
+    weaver add client
     `;
   }
   if (!hasQueries) {
@@ -55,5 +87,9 @@ const parse = (_argv) => {
 };
 
 module.exports = {
+  _hasValidQueries,
+  _hasValidDataClient,
+  _hasValidDataClientss,
+  _isValidConfig,
   parse,
 };
