8b11f2b64da8adf54a46119ee803644ef7ae5177
SkeloGH <dev.aaron.gh@gmail.com>
2022-11-13 13:43:37 -0600

    fixed export

--- bin/commands/run.js
+++ bin/commands/run.js
@@ -1,8 +1,8 @@
-const { parse } = require('./run/')
+const { parse } = require('./run/');
 
 module.exports = {
   name: 'run',
   description: 'Runs the app with the loaded configuration',
   setup: (yargs) => yargs,
-  parse,
+  parse: parse,
 };
