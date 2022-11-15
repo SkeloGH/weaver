59dbe63a85ad99cbdc3db2eab3ea873f42a63162
SkeloGH <dev.aaron.gh@gmail.com>
2022-11-14 23:32:48 -0600

    decoupled db initialization for mongodb

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,60 +1,30 @@
 const logging = require('debug');
-const ObjectId = require('bson-objectid');
-const CONFIG = require('./config');
-const Weaver = require('../');
 const ldObject = require('lodash/object');
 
+const {
+  disconnectMongoDB,
+  initializeAndSeedMongoDB,
+  mockCart,
+  mockOrder,
+  mockUser,
+} = require('./__shared__');
+const CONFIG = require('./config');
+const Weaver = require('..');
+
 const log = logging('Weaver:__tests__:root');
+let targetClient1;
+
+beforeAll(async () => {
+  log('Initializing dataClients');
+  [targetClient1] = CONFIG.dataClients;
+  await initializeAndSeedMongoDB();
+});
+
+afterAll(async () => {
+  await disconnectMongoDB();
+});
 
 describe('Weaver main test suite', () => {
-  let sourceClient1;
-  let targetClient1;
-
-  const mockUser = { _id: ObjectId('abcdef78901234abcdef1234'), name: 'John', orders: [{ orderId: '4321fedcbafedcba67890123' }] };
-  const mockCart = { _id: ObjectId('fedcba67890123fedcba4321'), userId: 'abcdef78901234abcdef1234' };
-  const mockOrder = { _id: ObjectId('4321fedcbafedcba67890123'), cartId: 'fedcba67890123fedcba4321' };
-
-  beforeAll(async () => {
-    log('Initializing dataClients');
-    [sourceClient1, targetClient1] = CONFIG.dataClients;
-
-    await sourceClient1.connect();
-    await targetClient1.connect();
-
-    const users = sourceClient1.db.collection('users');
-    const carts = sourceClient1.db.collection('carts');
-    const orders = sourceClient1.db.collection('orders');
-
-    await users.insertOne(mockUser);
-    await carts.insertOne(mockCart);
-    await orders.insertOne(mockOrder);
-
-    /**
-     * @note this call to _fetchCollections is needed to reload the collections
-     * since it runs when `connect` is called above, right before loading the mocked
-     * collections
-     */
-    const sourcecollections = await sourceClient1._fetchCollections();
-
-    log('Mock data created', sourcecollections);
-
-    const insertedUser = await users.findOne(mockUser);
-    const insertedCart = await carts.findOne(mockCart);
-    const insertedOrder = await orders.findOne(mockOrder);
-
-    expect(ldObject.omit('_id', insertedUser)).toEqual(ldObject.omit('_id', mockUser));
-    expect(ldObject.omit('_id', insertedCart)).toEqual(ldObject.omit('_id', mockCart));
-    expect(ldObject.omit('_id', insertedOrder)).toEqual(ldObject.omit('_id', mockOrder));
-    await sourceClient1.database.close();
-    await targetClient1.database.close();
-  });
-
-  afterAll(async () => {
-    [sourceClient1, targetClient1] = CONFIG.dataClients;
-    await sourceClient1.disconnect();
-    await targetClient1.disconnect();
-  });
-
   test('Public methods are defined', () => {
     const weaver = new Weaver(CONFIG);
     expect(weaver.collect).toBeDefined();
@@ -87,9 +57,10 @@ describe('Weaver main test suite', () => {
     const mockClients = [client];
     const results = await weaver.connectClients(mockClients);
     expect(results).toEqual(['works']);
+    await weaver.disconnect();
   });
 
-  test('Weaver interwines', async (done) => {
+  test('Weaver interwines', async () => {
     const weaver = new Weaver(CONFIG);
     const opResult = await weaver.run();
 
@@ -116,5 +87,6 @@ describe('Weaver main test suite', () => {
     expect(ldObject.omit('_id', insertedUser)).toEqual(ldObject.omit('_id', mockUser));
     expect(ldObject.omit('_id', insertedCart)).toEqual(ldObject.omit('_id', mockCart));
     expect(ldObject.omit('_id', insertedOrder)).toEqual(ldObject.omit('_id', mockOrder));
+    await weaver.disconnect();
   });
 });
